346f8fe3ad0004adbd346122a70ca886babf22d3
Marc-Antoine Parent <maparent@acm.org>
2017-06-05 00:53:14 -0400

    spasql in unicode queries

--- src/virtuoso.cpp
+++ src/virtuoso.cpp
@@ -1,6 +1,8 @@
 #include "pyodbc.h"
 #include "virtuoso.h"
 #include "pyodbccompat.h"
+#include <wctype.h>
+#include <wchar.h>
 
 bool
 isVirtuoso(HDBC hdbc)
@@ -22,15 +24,34 @@ isVirtuoso(HDBC hdbc)
 bool
 isSPASQL(PyObject *pSql)
 {
-    // check if it's a string or unicode?
-    char *query = PyBytes_AS_STRING(pSql);
+  #if PY_MAJOR_VERSION < 3
+    if (PyBytes_Check(pSql))
+    {
+        char *query = PyBytes_AS_STRING(pSql);
+        if (!query)
+            return false;
+        while (*query && isspace(*query))
+            query++;
+        return !strncasecmp(query, "SPARQL", 6);
+    }
+  #endif
+    if (PyUnicode_Check(pSql)) {
+        // take a reasonable prefix size
+        wchar_t prefix[32];
+        wchar_t* query = prefix;
+      #if PY_MAJOR_VERSION < 3
+        Py_ssize_t size = PyUnicode_AsWideChar((PyUnicodeObject*)pSql, query, 32);
+      #else
+        Py_ssize_t size = PyUnicode_AsWideChar(pSql, query, 32);
+      #endif
 
-    if (!query)
-	return false;
-    while (*query && isspace(*query))
-	query++;
-
-    if (!strncasecmp(query, "SPARQL", 6))
-	return true;
+        while (*query && iswspace(*query)) {
+            query++;
+            size--;
+        }
+        if (size < 6)
+            return false;
+        return !wcsncasecmp(query, L"SPARQL", 6);
+    }
     return false;
 }
